8953d4e2cb3fa4f69db17b4c34aead195b47a096
Vika-meow <viktoriagnezdilova@gmail.com>
2021-04-12 21:56:51 +0700

    V1_100

--- run_100K_V1.sh
+++ run_100K_V1.sh
@@ -11,29 +11,40 @@ echo "data folder: " ${data_folder}
 echo "log folder: " ${log_folder}
 cur_time="`date +%Y%m%d%H%M%S`"
 
-
-target_dataset='DBP_en_WD_en_100K_V1'
+target_dataset='DBP_en_DBP_ru_100K_V1'
 python3 main.py \
     --target_dataset ${target_dataset} \
     --KG1_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_DBP_en' \
     --KG1_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_DBP_en' \
-    --KG2_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_WD_en' \
-    --KG2_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_WD_en' \
-    --ent_link_path ${data_folder}'processed_data/ent_links/ent_links_DBP_en_WD_en' \
+    --KG2_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_DBP_ru' \
+    --KG2_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_DBP_ru' \
+    --ent_link_path ${data_folder}'processed_data/ent_links/ent_links_DBP_en_DBP_ru' \
     --ent_link_num 100000 \
-    --init_speed 0.35 \
+    --init_speed 0.25 \
     --pre_delete 0 >> ${data_folder}sampled_data/log/${target_dataset}_${cur_time}.log
 
+#target_dataset='DBP_en_WD_en_100K_V1'
+#python3 main.py \
+#    --target_dataset ${target_dataset} \
+#    --KG1_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_DBP_en' \
+#    --KG1_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_DBP_en' \
+#    --KG2_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_WD_en' \
+#    --KG2_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_WD_en' \
+#    --ent_link_path ${data_folder}'processed_data/ent_links/ent_links_DBP_en_WD_en' \
+#    --ent_link_num 100000 \
+#    --init_speed 0.35 \
+#    --pre_delete 0 >> ${data_folder}sampled_data/log/${target_dataset}_${cur_time}.log
 
-target_dataset='DBP_en_YG_en_100K_V1'
-python3 main.py \
-    --target_dataset ${target_dataset} \
-    --KG1_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_DBP_en' \
-    --KG1_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_DBP_en' \
-    --KG2_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_YG_en' \
-    --KG2_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_YG_en' \
-    --ent_link_path ${data_folder}'processed_data/ent_links/ent_links_DBP_en_YG_en' \
-    --ent_link_num 100000 \
-    --init_speed 0.15 \
-    --pre_delete 0 >> ${data_folder}sampled_data/log/${target_dataset}_${cur_time}.log
+
+#target_dataset='DBP_en_YG_en_100K_V1'
+#python3 main.py \
+#    --target_dataset ${target_dataset} \
+#    --KG1_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_DBP_en' \
+#    --KG1_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_DBP_en' \
+#    --KG2_rel_triple_path ${data_folder}'processed_data/rel_triples/rel_triples_YG_en' \
+#    --KG2_attr_triple_path ${data_folder}'processed_data/attr_triples/attr_triples_YG_en' \
+#    --ent_link_path ${data_folder}'processed_data/ent_links/ent_links_DBP_en_YG_en' \
+#    --ent_link_num 100000 \
+#    --init_speed 0.15 \
+#    --pre_delete 0 >> ${data_folder}sampled_data/log/${target_dataset}_${cur_time}.log
 
